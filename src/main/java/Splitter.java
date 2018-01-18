fec0f7a9e90786924e69a6caa6b5d46390485429
bipinkh <bipinkh91@gmail.com>
2018-01-18 13:22:55 +0545

    merge file parts function

--- src/main/java/Splitter.java
+++ src/main/java/Splitter.java
@@ -1,3 +1,6 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,14 +30,33 @@ public class Splitter {
         return fileParts;
     }
 
-    public static void main(String[] args){
-        System.out.println(System.getProperty("user.dir"));
-//        List<byte[]> parts = split(new File("src//main//resources//testfile.txt"));
-        List<byte[]> parts = split(new File("src//main//resources//MileyCyrus.mp3"));
-        System.out.println("Total parts :: "+ parts.size());
-        for (int i = 0; i<= parts.size(); i++){
-            System.out.println("Part "+String.valueOf(i)+"\n"+ new String(parts.get(0)));
-//            System.out.println("Part "+String.valueOf(i)+"\n"+ parts.get(0));
+    public static boolean merge(List<byte[]> fileParts, File into){
+        try (FileOutputStream fos = new FileOutputStream(into) ) {
+            for (byte[] part : fileParts){
+                fos.write(part);
+            }
+        }catch (Exception e){
+            System.out.println("could not merge the files :\n"+e.getMessage());
+            return false;
         }
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        String fileName = "src//main//resources//testfile.txt";
+
+//        List<byte[]> parts = split(new File());
+
+        List<byte[]> parts = split(new File(fileName));
+        System.out.println("Total parts :: "+ parts.size());
+
+//        for (int i = 0; i<= parts.size(); i++){
+//            System.out.println("Part "+String.valueOf(i)+"\n"+ new String(parts.get(0)));
+////            System.out.println("Part "+String.valueOf(i)+"\n"+ parts.get(0));
+//        }
+
+
+
     }
 }
