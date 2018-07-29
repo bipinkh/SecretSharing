6a895717a93c2c1bc1ab1d2d07faff907aa9325e
bipinkh <bipinkh91@gmail.com>
2018-07-29 19:14:08 +0545

    updated algorithms and resolved bug

--- src/main/java/fileshares/TestFile.java
+++ src/main/java/fileshares/TestFile.java
@@ -10,11 +10,11 @@ public class TestFile {
 
 	public static void main(String[] args) throws Exception {
 
-//	    String baseFolder = "src/main/java/fileshares/imagefiles/";
-//        String inputFilepath = "src/main/java/fileshares/imagefiles/logo.jpg";
-//        String sendEncFilePath = "src/main/java/fileshares/imagefiles/sendEncLogo.jpg";
-//        String receiveEncFilePath = "src/main/java/fileshares/imagefiles/receivedEncLogo.jpg";
-//        String decryptedFile = "src/main/java/fileshares/imagefiles/final.jpg";
+	    String baseFolder = "src/main/java/fileshares/imagefiles/";
+        String inputFilepath = "src/main/java/fileshares/imagefiles/logo.jpg";
+        String sendEncFilePath = "src/main/java/fileshares/imagefiles/sendEncLogo.jpg";
+        String receiveEncFilePath = "src/main/java/fileshares/imagefiles/receivedEncLogo.jpg";
+        String decryptedFile = "src/main/java/fileshares/imagefiles/final.jpg";
 
 //        String baseFolder = "src/main/java/fileshares/music/";
 //        String inputFilepath = "src/main/java/fileshares/music/MileyCyrus.mp3";
@@ -22,11 +22,11 @@ public class TestFile {
 //        String receiveEncFilePath = "src/main/java/fileshares/music/receivedEncLogo.mp3";
 //        String decryptedFile = "src/main/java/fileshares/music/final.mp3";
 
-        String baseFolder = "src/main/java/fileshares/textfiles/";
-        String inputFilepath = "src/main/java/fileshares/textfiles/originalText.txt";
-        String sendEncFilePath = "src/main/java/fileshares/textfiles/sendEncLogo.txt";
-        String receiveEncFilePath = "src/main/java/fileshares/textfiles/receivedEncLogo.txt";
-        String decryptedFile = "src/main/java/fileshares/textfiles/final.txt";
+//        String baseFolder = "src/main/java/fileshares/textfiles/";
+//        String inputFilepath = "src/main/java/fileshares/textfiles/originalText.txt";
+//        String sendEncFilePath = "src/main/java/fileshares/textfiles/sendEncLogo.txt";
+//        String receiveEncFilePath = "src/main/java/fileshares/textfiles/receivedEncLogo.txt";
+//        String decryptedFile = "src/main/java/fileshares/textfiles/final.txt";
 
         String pw = "iGotThisOK";   // password
 		SecretKey masterAESkey = DigitalEnvelope.createMasterSecretKey(pw);
